aadd88cd38d481ede569efae5f21b9d3c8f12d5c
patryk <patryk.kachel@limestreet.pl>
2017-09-02 13:38:33 +0200

    some unneeded functions removed

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -7,68 +7,6 @@
  * @package blogito
  */
 
-if ( ! function_exists( 'the_posts_navigation' ) ) :
-
-	/**
-	 * Display navigation to next/previous set of posts when applicable.
-	 *
-	 * @todo Remove this function when WordPress 4.3 is released.
-	 */
-	function the_posts_navigation() {
-	// Don't print empty markup if there's only one page.
-	if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
-			return;
-	}
-	?>
-	<nav class="navigation posts-navigation" role="navigation">
-		<h2 class="screen-reader-text"><?php esc_html_e( 'Posts navigation', 'blogito' ); ?></h2>
-		<div class="nav-links">
-
-		<?php if ( get_next_posts_link() ) : ?>
-			<div class="nav-previous"><?php next_posts_link( esc_html__( 'Older posts', 'blogito' ) ); ?></div>
-		<?php endif; ?>
-
-		<?php if ( get_previous_posts_link() ) : ?>
-			<div class="nav-next"><?php previous_posts_link( esc_html__( 'Newer posts', 'blogito' ) ); ?></div>
-		<?php endif; ?>
-
-		</div><!-- .nav-links -->
-	</nav><!-- .navigation -->
-	<?php
-	}
-
-endif;
-
-if ( ! function_exists( 'the_post_navigation' ) ) :
-
-	/**
-	 * Display navigation to next/previous post when applicable.
-	 *
-	 * @todo Remove this function when WordPress 4.3 is released.
-	 */
-	function the_post_navigation() {
-	// Don't print empty markup if there's nowhere to navigate.
-	$previous = ( is_attachment() ) ? get_post( get_post()->post_parent ) : get_adjacent_post( false, '', true );
-	$next = get_adjacent_post( false, '', false );
-
-	if ( ! $next && ! $previous ) {
-			return;
-	}
-	?>
-	<nav class="navigation post-navigation" role="navigation">
-		<h2 class="screen-reader-text"><?php esc_html_e( 'Post navigation', 'blogito' ); ?></h2>
-		<div class="nav-links">
-		<?php
-		previous_post_link( '<div class="nav-previous">%link</div>', '%title' );
-		next_post_link( '<div class="nav-next">%link</div>', '%title' );
-		?>
-		</div><!-- .nav-links -->
-	</nav><!-- .navigation -->
-	<?php
-	}
-
-endif;
-
 if ( ! function_exists( 'blogito_posted_on' ) ) :
 
 	/**
@@ -626,7 +564,7 @@ if ( ! function_exists( 'blogito_comment' ) ) :
 	add_filter( 'walker_nav_menu_start_el', 'blogito_submenu_span', 10, 4 );
 
 	/**
-	 * Function for displaying related posts on single page.
+	 * Function for displaying related posts on single post.
 	 */
 	function blogito_related_posts() {
 	$show_related = get_theme_mod( 'single_page_related_posts_show', 1 );
@@ -715,5 +653,3 @@ if ( ! function_exists( 'blogito_comment' ) ) :
 	}
 
 	add_action( 'init', 'blogito_jetpack_sharing_remove_filters' );
-
-
